6dc05bfddf77884459e2afa94d630f68769ae90d
Colby Klein <shakesoda@gmail.com>
2016-12-14 02:54:59 -0800

    add additive animation blending and playback rates, removed send_pose.

--- anim9.lua
+++ anim9.lua
@@ -3,7 +3,7 @@ local cpml = require "cpml"
 local anim = {
 	_LICENSE     = "anim9 is distributed under the terms of the MIT license. See LICENSE.md.",
 	_URL         = "https://github.com/excessive/anim9",
-	_VERSION     = "0.0.3",
+	_VERSION     = "0.1.0",
 	_DESCRIPTION = "Animation library for LÖVE3D.",
 }
 anim.__index = anim
@@ -16,16 +16,53 @@ local function calc_bone_matrix(pos, rot, scale)
 		:scale(out, scale)
 end
 
-local function calc_pose(skeleton, base, p1, p2, position)
+local function bind_pose(skeleton)
+	local pose = {}
+	for i = 1, #skeleton do
+		pose[i] = {
+			translate = skeleton[i].position,
+			rotate    = skeleton[i].rotation,
+			scale     = skeleton[i].scale
+		}
+	end
+	return pose
+end
+
+local function add_poses(skeleton, p1, p2)
+	local new_pose = {}
+	for i = 1, #skeleton do
+		local inv = p1[i].rotate:clone()
+		inv:inverse(inv)
+		new_pose[i] = {
+			translate = p1[i].translate + (p2[i].translate - p1[i].translate),
+			rotate    = (p2[i].rotate * inv) * p1[i].rotate,
+			scale     = p1[i].scale + (p2[i].scale - p1[i].scale)
+		}
+	end
+	return new_pose
+end
+
+local function mix_poses(skeleton, p1, p2, weight)
+	local new_pose = {}
+	for i = 1, #skeleton do
+		local r = cpml.quat():slerp(p1[i].rotate, p2[i].rotate, weight)
+		r:normalize(r)
+		new_pose[i] = {
+			translate = cpml.vec3():lerp(p1[i].translate, p2[i].translate, weight),
+			rotate    = r,
+			scale     = cpml.vec3():lerp(p1[i].scale, p2[i].scale, weight)
+		}
+	end
+	return new_pose
+end
+
+local function update_matrices(skeleton, base, pose)
 	local animation_buffer = {}
 	local transform = {}
 	local bone_lookup = {}
+
 	for i, joint in ipairs(skeleton) do
-		local t = cpml.vec3():lerp(p1[i].translate, p2[i].translate, position)
-		local r = cpml.quat():slerp(p1[i].rotate, p2[i].rotate, position)
-		local s = cpml.vec3():lerp(p1[i].scale, p2[i].scale, position)
-		r:normalize(r)
-		local m = calc_bone_matrix(t, r, s)
+		local m = calc_bone_matrix(pose[i].translate, pose[i].rotate, pose[i].scale)
 		local render
 
 		if joint.parent > 0 then
@@ -48,15 +85,11 @@ local function new(data, anims)
 	if not data.skeleton then return end
 
 	local t = {
-		current_animation = false,
-		current_callback  = false,
-		current_time      = 0,
-		current_frame     = 1,
-		current_marker    = 0,
-		animations        = {},
-		playing           = false,
-		skeleton          = data.skeleton,
-		inverse_base      = {}
+		active       = {},
+		animations   = {},
+		skeleton     = data.skeleton,
+		inverse_base = {},
+		bind_pose    = bind_pose(data.skeleton)
 	}
 
 	-- Calculate inverse base pose.
@@ -97,110 +130,132 @@ function anim:add_animation(animation, frame_data)
 	self.animations[new_anim.name] = new_anim
 end
 
-function anim:reset()
-	self.current_animation = false
-	self.current_time      = 0
-	self.playing           = false
+local function new_animation(name, weight, rate, callback)
+	return {
+		name     = assert(name),
+		frame    = 1,
+		time     = 0,
+		marker   = 0,
+		rate     = rate or 1,
+		weight   = weight or 1,
+		callback = callback or false,
+		playing  = true
+	}
 end
 
-function anim:play(name, callback, stopped)
-	self.current_animation = name
-	self.current_callback  = callback
-	self.playing = stopped == nil and true or not stopped
+function anim:reset(name)
+	if not self.active[name] then return end
+	self.active[name].time   = 0
+	self.active[name].marker = 0
+	self.active[name].frame  = 1
 end
 
-function anim:pause(toggle)
-	self.playing = toggle == nil and false or not self.playing
+function anim:play(name, weight, rate, callback)
+	if self.active[name] then return end
+	assert(self.animations[name], string.format("Invalid animation: '%s'", name))
+	self.active[name] = new_animation(name, weight, rate, callback)
+	table.insert(self.active, self.active[name])
 end
 
-function anim:stop()
-	self:pause()
-	self:reset()
+function anim:pause(name)
+	if not self.active[name] then return end
+	self.active[name].playing = not self.active[name].playing
 end
 
-function anim:length(aname)
-	aname = aname or self.current_animation
-	local _anim = self.animations[aname]
-	assert(_anim, string.format("Invalid animation: \'%s\'", aname))
-	return _anim.length / _anim.framerate
-end
-
-function anim:step(reverse)
-	if self.current_animation and not self.playing then
-		local _anim = self.animations[self.current_animation]
-		local length = _anim.length / _anim.framerate
-
-		if reverse then
-			self.current_time = self.current_time - (1/_anim.framerate)
-		else
-			self.current_time = self.current_time + (1/_anim.framerate)
+function anim:stop(name)
+	if not self.active[name] then return end
+	self.active[name] = nil
+	for i, v in ipairs(self.active) do
+		if v.name == name then
+			table.remove(self.active, i)
+			break
 		end
-
-		if _anim.loop then
-			if self.current_time < 0 then
-				self.current_time = self.current_time + length
-			end
-			self.current_time = cpml.utils.wrap(self.current_time, length)
-		else
-			if self.current_time < 0 then
-				self.current_time = 0
-			end
-			self.current_time = math.min(self.current_time, length)
-		end
-
-		local position = self.current_time * _anim.framerate
-		local frame = _anim.frames[math.floor(position)+1]
-
-		-- Update the final pose
-		self.current_pose, self.current_matrices = calc_pose(
-			self.skeleton, self.inverse_base,
-			frame, frame, 0
-		)
 	end
 end
 
+function anim:length(aname)
+	local _anim = assert(self.animations[_anim], string.format("Invalid animation: \'%s\'", aname))
+	return _anim.length / _anim.framerate
+end
+
+function anim:step(name, reverse)
+	assert(self.active[name], string.format("Invalid animation: '%s'", name))
+	local _anim = self.animations[name]
+	local length = _anim.length / _anim.framerate
+	local meta = self.active[name]
+
+	if reverse then
+		meta.time = meta.time - (1/_anim.framerate)
+	else
+		meta.time = meta.time + (1/_anim.framerate)
+	end
+
+	if _anim.loop then
+		if meta.time < 0 then
+			meta.time = meta.time + length
+		end
+		meta.time = cpml.utils.wrap(meta.time, length)
+	else
+		if meta.time < 0 then
+			meta.time = 0
+		end
+		meta.time = math.min(meta.time, length)
+	end
+
+	local position = self.current_time * _anim.framerate
+	local frame = _anim.frames[math.floor(position)+1]
+	meta.frame = frame
+
+	-- Update the final pose
+	local pose = mix_poses(self.skeleton, frame, frame, 0)
+	self.current_pose, self.current_matrices = update_matrices(
+		self.skeleton, self.inverse_base, pose
+	)
+end
+
 function anim:update(dt)
-	if self.current_animation and self.playing then
-		local _anim = self.animations[self.current_animation]
-		assert(_anim, string.format("Invalid animation: %s", self.current_animation))
+	if #self.active == 0 then
+		return
+	end
+
+	local pose = self.bind_pose
+	for _, meta in ipairs(self.active) do
+		local _anim = self.animations[meta.name]
 		local length = _anim.length / _anim.framerate
-		self.current_time = self.current_time + dt
-		if self.current_time >= length then
-			if type(self.current_callback) == "function" then
-				self.current_callback(self)
+		meta.time = meta.time + dt * meta.rate
+		if meta.time >= length then
+			if type(meta.callback) == "function" then
+				meta.callback(self)
 			end
 		end
 
 		-- If we're not looping, we just want to leave the animation at the end.
 		if _anim.loop then
-			self.current_time = cpml.utils.wrap(self.current_time, length)
+			meta.time = cpml.utils.wrap(meta.time, length)
 		else
-			self.current_time = math.min(self.current_time, length)
+			meta.time = math.min(meta.time, length)
 		end
 
-		local position = self.current_time * _anim.framerate
+		local position = meta.time * _anim.framerate
 		local f1, f2 = math.floor(position), math.ceil(position)
 		position = position - f1
 		f2 = f2 % (_anim.length)
 
 		-- Update the final pose
-		self.current_pose, self.current_matrices = calc_pose(
-			self.skeleton, self.inverse_base,
-			_anim.frames[f1+1], _anim.frames[f2+1], position
+		local interp = mix_poses(
+			self.skeleton,
+			_anim.frames[f1+1],
+			_anim.frames[f2+1],
+			position
 		)
+		local mix = mix_poses(self.skeleton, pose, interp, meta.weight)
+		pose = add_poses(self.skeleton, pose, mix)
+	end
 
-		self.current_frame = f1
-	end
-end
-
-function anim:send_pose(shader, uniform, toggle_uniform)
-	if not self.current_pose then
-		return
-	end
-	shader:send(uniform, unpack(self.current_pose))
-	if toggle_uniform then
-		shader:sendInt(toggle_uniform, 1)
-	end
+	self.current_pose, self.current_matrices = update_matrices(
+		self.skeleton, self.inverse_base, pose
+	)
+	self.current_frame = f1
 end
 
 return setmetatable({
